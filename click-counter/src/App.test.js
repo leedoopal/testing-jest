cb0877962875f3265a24f0cc7b48656ed95b6c2a
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-27 21:19:53 +0900

    code refactoring

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -5,21 +5,30 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * App component를 위한 ShallowWrapper function 생성
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = () => shallow(<App />);
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-button']");
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
   expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
   expect(counterDisplay.length).toBe(1);
 });
 
