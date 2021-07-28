17103b80782628825b7a84a508ea146e27abac3c
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-28 18:10:57 +0900

    setup jotto app, Congrats component 생성

---
+++ jotto/src/Congrats.test.js
@@ -0,0 +1,8 @@
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import Congrats from './Congrats';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {});
