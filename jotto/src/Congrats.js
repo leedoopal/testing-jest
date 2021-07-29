3bd57bc93a67cd6dd02e9a4dc9690dada3c43fdc
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-29 16:50:57 +0900

    Congrats component 코드 추가

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -1,3 +1,24 @@
-export default function Congrats({ success }) {
-  return <div />;
-}
+import React from 'react';
+
+/**
+ * 축하하는 메시지를 위한 함수형 리액트 컴포넌트
+ * @function
+ * @param {object} props - react props
+ * @returns {JSX.Element} - 렌더된 컴포넌트
+ */
+
+const Congrats = ({ success }) => {
+  if (success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! 단어를 추측하였습니다!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats">다시 한번 찾아보세요</div>;
+  }
+};
+
+export default Congrats;
