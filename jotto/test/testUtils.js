6e58857f9167d066ef6663c2521316926c9ac160
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-29 14:18:21 +0900

    Congrats 컴포넌트 테스트 setup

---
+++ jotto/test/testUtils.js
@@ -0,0 +1,9 @@
+/**
+ * 지정된 data-test 속성을 가진 node(s)를 리턴
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper
+ * @param {string} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
