0791e992dcd0f2f10e3949881ca3914d4f70102c
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-08-02 22:21:59 +0900

    GuessWords 컴포넌트와 테스트파일 셋팅

---
+++ jotto/src/GuessWords.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessWords = (props) => {
+  return <div></div>;
+};
+
+GuessWords.propTypes = {};
+
+export default GuessWords;
