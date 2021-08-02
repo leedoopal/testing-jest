efca379188f4fe2ddc0fc441a2a95fe8d695e267
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-08-02 22:33:33 +0900

    'no words guessed'를 위한 code

--- jotto/src/GuessWords.js
+++ jotto/src/GuessWords.js
@@ -2,7 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GuessWords = (props) => {
-  return <div></div>;
+  let contents;
+  if (props.guessWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">
+        시크릿 단어를 다시 추측해보세요!
+      </span>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
 };
 
 GuessWords.propTypes = {
