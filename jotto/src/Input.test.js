9736272413a6c2442ae635c989ca64d863a147df
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-08-03 15:33:08 +0900

    Input component 추가

---
+++ jotto/src/Input.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr } from '../test/testUtils';
+import Input from './Input';
+
+/**
+ * app component를 위한 setup function
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<Input />);
+};
+
+test('Input renders without error', () => {
+  const wrapper = setup();
+  const inputComponent = findByTestAttr(wrapper, 'component-input');
+  expect(inputComponent.length).toBe(1);
+});
