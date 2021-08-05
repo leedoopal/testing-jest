3edcf69912d6eb6aa875449b2c32e35ce6a3db8f
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-08-05 16:50:07 +0900

    guess word를 위한 함수형 테스트 추가

--- jotto/src/guessWord.test.js
+++ jotto/src/guessWord.test.js
@@ -28,7 +28,7 @@ const setup = (state = {}) => {
   return wrapper;
 };
 
-describe('no words guessed', () => {
+describe.skip('no words guessed', () => {
   let wrapper;
 
   beforeEach(() => {
@@ -45,6 +45,56 @@ describe('no words guessed', () => {
   });
 });
 
-describe('some words guessed', () => {});
+describe.skip('some words guessed', () => {
+  let wrapper;
 
-describe('guess secret word', () => {});
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessWords: [{ guessedWord: 'agile', letterMatchCount: 1 }],
+    });
+  });
+
+  test('creates GuessedWords table with one row', () => {
+    const guessedWordRows = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordRows).toHaveLength(2);
+  });
+});
+
+describe.skip('guess secret word', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessWords: [{ guessWord: 'agile', letterMatchCount: 1 }],
+    });
+
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    const mockEvent = { target: { value: 'party' } };
+    inputBox.simulate('change', mockEvent);
+
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click', { preventDefault() {} });
+  });
+
+  test('adds row to guessedWords table', () => {
+    const guessedWordRows = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordRows).toHaveLength(3);
+  });
+
+  test('displays congrats component', () => {
+    const congrats = findByTestAttr(wrapper, 'component-congrats');
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+
+  test('does not display input component contents', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+    expect(inputBox.exists()).toBe(false);
+
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    expect(submitButton.exists()).toBe(false);
+  });
+});
